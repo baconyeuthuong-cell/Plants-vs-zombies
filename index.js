6e7c76bbb9865560a7671a7a177da465433aa021
baconyeuthuong-cell <baconyeuthuong@gmail.com>
2025-12-18 12:32:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("JS OK");
+const startBtn = document.getElementById("start");
+const minigamecomingsoonBtn = document.getElementById("minigamecomingsoon");
+const exitBtn = document.getElementById("exit");
+
+start.onclick = () => {
+  window.location.href = "adventure/manchoi.html";
+};
+minigamecomingsoon.onclick = () => {
+ window.location.href = "minigame/minigame.html";
+};
+exit.onclick = () => {
+  alert("Exit r\u1ED3i th\u00EC t\u1EAFt tab \u0111i");
+};
