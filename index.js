b66df7f4288ff1bfeb510330a27ff3ae1007f698
XuanPhongDepZai <baconyeuthuong@gmail.com>
2025-12-19 11:43:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const exitBtn = document.getElementById("exit");
-exitBtn.onclick = () => {
-  alert("Exit rồi thì tắt tab đi");
+const startBtn = document.getElementById("start"); 
+const minigamecomingsoonBtn = document.getElementById("minigamecomingsoon"); 
+const exitBtn = document.getElementById("exit"); 
+startBtn.onclick = () => { 
+  window.location.href = "adventure/manchoi.html"; 
+}; 
+minigamecomingsoonBtn.onclick = () => { 
+  window.location.href = "minigame/minigame.html"; 
+};
+exitBtn.onclick = () => { 
+  alert("Exit rồi thì tắt tab đi"); 
 };
